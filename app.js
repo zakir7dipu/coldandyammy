8747c6788bb2cddc0bcf87ccabd3b9e1d576481e
zakir7dipu <zakir67@dipti.com.bd>
2022-04-02 10:07:22 +0600

    update

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const sequelize = require('./database');
 const Icream = require('./model/icream');
+const User = require('./model/user');
+
+Icream.hasMany(User);
+User.belongsTo(Icream);
 
 sequelize.sync(
-    {force:true}
+    // {force:true}
 ).then(()=>console.log('db is connected'))
 
 const app = express();
@@ -28,7 +32,7 @@ app.get('/icream/:id', async (req, res) => {
 app.put('/icream/:id', async (req, res) => {
     const requestId = req.params.id;
     const icream = await Icream.findOne({where:{id: requestId}});
-    icream.name = 'Vanilla'
+    icream.name = req.body.name
     await icream.save();
     res.send(icream)
 })
@@ -37,6 +41,22 @@ app.delete('/icream/:id', async (req, res) => {
     const requestId = req.params.id;
     await Icream.destroy({where:{id: requestId}});
     res.send('Deleted')
+})
+
+app.post('/user', async (req, res) => {
+    const requestId = req.body.icream;
+    const icream = await Icream.findOne({where:{id: requestId}});
+    let userData = {
+        name: req.body.name,
+        email: req.body.email,
+    }
+    await icream.createUser(userData)
+    res.send('User Stored');
+})
+
+app.get('/users', async (req, res) => {
+    let users = await User.findAll()
+    res.send(users);
 })
 
 app.listen(3131, () => {
---
+++ model/user.js
@@ -0,0 +1,20 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../database')
+
+class User extends Model {}
+
+User.init({
+    name:{
+        type: DataTypes.STRING,
+        // allowNull: false,
+    },
+    email:{
+        type: DataTypes.STRING,
+        // allowNull: false,
+    }
+},{
+    sequelize,
+    modelName: 'user'
+})
+
+module.exports = User
